c360db627df503f571f08355e198d1282160e535
Ender Bonnet <enbonnet@gmail.com>
2019-10-13 17:47:28 -0300

    sociales en footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Icons } from "./icons"
 
 const Footer = () => {
   return (
@@ -8,6 +9,20 @@ const Footer = () => {
         {` `}
         <a href="https://www.gatsbyjs.org">Gatsby</a>
         {` `}y <a href="https://strapi.io/">Strapi</a>
+      </div>
+      <div className="social">
+        <a href="https://twitter.com/enbonnet">
+          <Icons name="twitter" />
+        </a>
+        <a href="https://www.linkedin.com/in/ender-bonnet-b47b25144/">
+          <Icons name="linkedin" />
+        </a>
+        <a href="https://instagram.com/enbonnet">
+          <Icons name="instagram" />
+        </a>
+        <a href="https://t.me/enbonnet">
+          <Icons name="sign" />
+        </a>
       </div>
     </footer>
   )
