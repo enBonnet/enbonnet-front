97a22cdc67dae4814bc3dcac7dee6bff75557531
Ender Bonnet <enbonnet@gmail.com>
2019-10-13 13:38:03 -0300

    modificando las typografias

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -3,6 +3,7 @@ import Typography from "typography"
 const typography = new Typography({
   baseFontSize: "18px",
   baseLineHeight: 1.666,
+  headerWeight: "normal",
   headerFontFamily: ["Space Mono", "monospace"],
   bodyFontFamily: ["Space Mono", "monospace"],
 })
