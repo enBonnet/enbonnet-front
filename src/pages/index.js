78904d0f7a99ca63a84326f70790448a38cca7d5
Ender Bonnet <enbonnet@gmail.com>
2019-10-12 22:48:53 -0300

    soporte para markdown

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Link, graphql } from "gatsby"
 import Img from "gatsby-image"
 import Layout from "../components/layout"
+import "../styles/global.css"
 
 const IndexPage = ({ data }) => (
   <Layout>
--- src/templates/article.js
+++ src/templates/article.js
@@ -1,13 +1,14 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Img from "gatsby-image"
+import ReactMarkdown from "react-markdown"
 import Layout from "../components/layout"
 
 const ArticleTemplate = ({ data }) => (
   <Layout>
     <h1>{data.strapiArticle.title}</h1>
     <Img fixed={data.strapiArticle.image.childImageSharp.fixed} />
-    <p>{data.strapiArticle.content}</p>
+    <ReactMarkdown source={data.strapiArticle.content} />
   </Layout>
 )
 
