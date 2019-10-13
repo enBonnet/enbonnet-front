eb1537907d3975f6fda3b17a096136e96651924a
Ender Bonnet <enbonnet@gmail.com>
2019-10-12 23:59:08 -0300

    add fount space mono

---
+++ src/utils/typography.js
@@ -0,0 +1,10 @@
+import Typography from "typography"
+
+const typography = new Typography({
+  baseFontSize: "18px",
+  baseLineHeight: 1.666,
+  headerFontFamily: ["Space Mono", "monospace"],
+  bodyFontFamily: ["Space Mono", "monospace"],
+})
+
+export default typography
