36bd7e366e498d8ce8345833e6e90a5b08eb9f5d
Ender Bonnet <enbonnet@gmail.com>
2019-10-14 22:22:58 -0300

    fix class

--- src/components/icons.js
+++ src/components/icons.js
@@ -14,7 +14,7 @@ const getUrl = name => {
           strokeWidth="2"
           strokeLinecap="round"
           strokeLinejoin="round"
-          class="feather feather-at-sign"
+          className="feather feather-at-sign"
         >
           <circle cx="12" cy="12" r="4"></circle>
           <path d="M16 8v5a3 3 0 0 0 6 0v-1a10 10 0 1 0-3.92 7.94"></path>
